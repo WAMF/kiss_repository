e9ec4eb9917455dd7e1af8878eb9c8a27e788b3b
defuera <denis@devartel.io>
2025-06-18 17:42:21 +0200

    chore: Clean up

--- test/integration/factories/inmemory_repository_factory.dart
+++ test/integration/factories/inmemory_repository_factory.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: avoid_print
 
 import 'package:kiss_repository/kiss_repository.dart';
-import '../../../../kiss_repository_tests/lib/test.dart';
 
+import '../../../../kiss_repository_tests/lib/test.dart';
 import 'inmemory_query_builder.dart';
 
 class InMemoryRepositoryFactory implements RepositoryFactory {
@@ -12,18 +12,8 @@ class InMemoryRepositoryFactory implements RepositoryFactory {
   static Future<void> initialize() async {
     if (_initialized) return;
 
-    // Reset global ID counter for clean tests
-    _resetIdCounter();
-
     print('✅ InMemory repository initialized');
     _initialized = true;
-  }
-
-  // Access the private _idCounter from InMemoryRepository via reflection or reset method
-  static void _resetIdCounter() {
-    // This is a workaround for the global counter - ideally the InMemoryRepository
-    // should provide a way to reset its counter, but for now we'll handle cleanup differently
-    print('🔄 InMemory ID counter reset');
   }
 
   @override
@@ -47,7 +37,6 @@ class InMemoryRepositoryFactory implements RepositoryFactory {
     }
 
     try {
-      // Get all items and delete them
       final allItems = await _repository!.query();
       print('🧹 Cleanup: Found ${allItems.length} items to delete');
 
